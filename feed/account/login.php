71a2185a1e64c8d25630069d36fdfa4a7f1ba46d
Alejandro Gonzalez <alexgve7@gmail.com>
2019-03-06 07:40:36 -0600

    Adding Login

---
+++ feed/account/login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+    <link rel="stylesheet" href="../../resources/public/css/materialize.min.css">
+    <link rel="stylesheet" href="../../resources/dashboard/privatelogin.css">
+    
+</head>
+<body>
+    
+    <div class="container">
+        <div class="row">
+            <div class="col s12 m12">
+                <div class="card red">
+                    <div class="card-content">
+                       
+                       <div class="card black">
+                        <div class="card-content">
+                            <p>Hola</p>
+                        </div>
+                       </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<script src="../../resources/public/js/jquery-3.2.1.min.js"></script>   
+<script src="../../resources/public/js/materialize.js"></script>   
+</body>
+</html>
